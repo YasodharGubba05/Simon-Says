b859b703f635924edbb63eb3f7ed592da3fbf474
YasodharGubba05 <83855085+YasodharGubba05@users.noreply.github.com>
2025-06-14 15:05:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 let userSeq=[];
 let gameSeq=[];
+let gameStarted=false;
+let level=0;
+let head2=document.querySelector('h2');
+let colors=["red","yellow","green","purple"];
 
 document.addEventListener("keypress",function(){
-    console.log("game started");
+    if(gameStarted==false) {
+        gameStarted=true;
+        console.log("game started");
+    }
+    levelup()
 })
+
+function levelup() {
+    level++;
+    head2.innerText=`level ${level}`;
+
+}
+
+function getRandomColor() {
+     let number = Math.floor(Math.random()*3);
+}
